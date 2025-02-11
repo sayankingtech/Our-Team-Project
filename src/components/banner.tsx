faa4582e42b78db4b66f240b736d180791a23a0a
Ataul molla <ataulmolla.itv@gmail.com>
2025-02-11 13:55:32 +0530

    new changes banner

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -1,25 +1,64 @@
-import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import image from "../assets/Images/Group-33704.webp"
+import "../App.css"
 
-// @ts-ignore
-// import { Swiper, SwiperSlide } from 'swiper/react';
-import {Swiper,SwiperSlide} from 'react-swiper';
+//@ts-ignore
+import 'swiper/css';
+import { Autoplay, Pagination } from 'swiper/modules';
 
 // @ts-ignore
 import 'swiper/css';
+// @ts-ignore
+import 'swiper/css/pagination';
 
-const banner: React.FC = () => {
+export default () => {
     return (
         <Swiper
-            spaceBetween={50}
-            slidesPerView={3}
+            // spaceBetween={50}
+            // slidesPerView={3}
+            loop={true}
             onSlideChange={() => console.log('slide change')}
+            onSwiper={(swiper) => console.log(swiper)}
+            autoplay={{
+                delay: 2000,
+                disableOnInteraction: false,
+            }}
+            pagination={{
+                dynamicBullets: true,
+            }}
+            modules={[Autoplay, Pagination]}
+            className="mySwiper"
         >
-            <SwiperSlide>Slide 1</SwiperSlide>
-            <SwiperSlide>Slide 2</SwiperSlide>
-            <SwiperSlide>Slide 3</SwiperSlide>
-            <SwiperSlide>Slide 4</SwiperSlide>
+            <SwiperSlide className='mb-[15px]'>
+                <div className=''>
+                    <img src={image} alt="" />
+                </div>
+            </SwiperSlide>
+            <SwiperSlide>
+                <div>
+                    <img src={image} alt="" />
+                </div>
+            </SwiperSlide>
+            <SwiperSlide>
+                <div>
+                    <img src={image} alt="" />
+                </div>
+            </SwiperSlide>
+            <SwiperSlide>
+                <div>
+                    <img src={image} alt="" />
+                </div>
+            </SwiperSlide>
+            <SwiperSlide>
+                <div>
+                    <img src={image} alt="" />
+                </div>
+            </SwiperSlide>
+            <SwiperSlide>
+                <div>
+                    <img src={image} alt="" />
+                </div>
+            </SwiperSlide>
         </Swiper>
-    )
-}
-
-export default banner;
+    );
+};
